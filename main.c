d2309fd04b430259487891b3bffe2681ed3aeae7
Akash G <akashgwin2002@gmail.com>
2021-09-28 19:32:13 +0530

    program to  add  three numbers

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>     
+
+int main()
+{
+    int a,b,c,d,sum;
+    printf("enter  the first number: ");
+    scanf("%d",&a);
+    printf("enter the second number: ");
+    scanf("%d",&b);
+    printf("enter the third number: ");
+    scanf("%d",&c);
+    printf("enter the fourth number: ");
+    scanf("%d",&d);
+    sum = a + b + c + d;
+    printf("the sum is: ");
+    return 0;
+}
